afa9c2c06e37a2e956d548598a9709e7e677bcf8
maryam <maryam79ah@gmail.com>
2026-09-03 14:40:20 +0300

    style: fix Pint formatting in post catalog and CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
-use Illuminate\Database\Seeder;
 use App\Support\PostCatalog;
+use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
